d980009dbaad1d8ce47ed8a028c14deb84ad50d3
Ben <benstevens2609@gmail.com>
2023-11-21 16:07:24 +0000

    removed unnecessary code

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from functions import *
+from plots import *
 
 data = np.loadtxt("bs521.txt", skiprows=2, max_rows=200)
 unosc_flux_prediction = np.loadtxt("bs521.txt", skiprows=204)
 
 bins = np.linspace(0, 20, 201)
-
-# plt.style.use("fast")
-# plt.figure()
-# plt.title("histogram of event energies")
-# plt.ylabel("number of events")
-# plt.xlabel("energy (GeV)")
-# plt.hist(bins[:-1], bins, weights=data)
-
-# plt.figure()
-# plt.title("predicted")
-# plt.ylabel("number of events")
-# plt.xlabel("energy (GeV)")
-# plt.hist(bins[:-1], bins, weights=unosc_flux_prediction)
-# plt.show()
-
-
-def muon_probability(theta, delta_m, length, energy):
-    """Return probability that muon neutrino is observed as muon neutrino"""
-    return 1 - (np.sin(2*theta)*np.sin(2*theta)*np.sin(1.267*delta_m*length/energy))
-
 
 theta = np.pi / 4
 delta_m = 2.4e-3
@@ -32,13 +15,14 @@ length = 295
 
 energy_vals = np.arange(0.5, 100, 0.5)
 
-p_vals = [muon_probability(theta, delta_m, length, i) for i in energy_vals]
+p_vals = [oscillation_probability(theta, delta_m, length, i) for i in energy_vals]
 
-plt.figure()
-plt.xlabel('energy (GeV')
-plt.ylabel('muon probability')
-plt.plot(energy_vals, p_vals, 'r')
-plt.show()
+# plt.figure()
+# plt.title("probability of not oscillating via equation (1)")
+# plt.xlabel('energy (GeV)')
+# plt.ylabel('muon probability')
+# plt.plot(energy_vals, p_vals, 'r'   )
+# plt.show()
 
 
 
