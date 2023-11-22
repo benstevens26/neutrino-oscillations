861d64436292122bba25035dbad184367e1bd594
Ben <benstevens2609@gmail.com>
2023-11-22 11:57:21 +0000

    added fig4() and fig5()

--- plots.py
+++ plots.py
@@ -16,7 +16,7 @@ mpl.rcParams['font.family'] = 'Arial'
 
 def fig1():
     """Histogram of event energies"""
-    bins = np.linspace(0, 20, 201)
+    bins = np.linspace(0, 10, 201)
     fig = plt.figure(figsize=(12, 8))
     plt.grid(alpha=0.3)
     plt.title("Histogram of event energies")
@@ -28,11 +28,11 @@ def fig1():
 
 
 def fig2():
-    """Histogram of predicted energies"""
-    bins = np.linspace(0, 20, 201)
+    """Histogram of simulated non-oscillating event energies"""
+    bins = np.linspace(0, 10, 201)
     fig = plt.figure(figsize=(12, 8))
     plt.grid(alpha=0.3)
-    plt.title("Histogram of non-oscillating event energies")
+    plt.title("Histogram of simulated non-oscillating event energies")
     plt.ylabel("Number of events")
     plt.xlabel("Energy (GeV)")
     plt.hist(bins[:-1], bins, weights=unosc_flux_prediction)
@@ -41,18 +41,81 @@ def fig2():
     plt.show()
 
 
-def fig3(theta=np.pi/4, delta_m=2.4e-3, length=295):
+def fig3(e_range, theta=np.pi/4, delta_m=2.4e-3, L=295):
     """Oscillation probability against energy"""
-    energy_vals = np.arange(0.5, 100, 0.5)
-    p_vals = [oscillation_probability(theta, delta_m, length, i) for i in energy_vals]
+    energy_vals = np.linspace(e_range[0], e_range[1], 100000)
+    u = [theta, delta_m]
+
+    p_vals = [oscillation_probability(u, L, i) for i in energy_vals]
 
     fig = plt.figure(figsize=(12, 8))
+    plt.grid(alpha=0.3)
     plt.title("Probability of not oscillating via equation (1)")
     plt.xlabel('Energy (GeV)')
     plt.ylabel('Muon probability')
-    plt.plot(energy_vals, p_vals, 'r'   )
+
+    plt.plot(energy_vals, p_vals, label="${\\theta}_{23} = \\pi/4$ \n"
+                                        "${\\Delta}m_{23}^{2} = 2.4$x$10^{-3}$ \n"
+                                        "$L = 295$")
+
+    plt.legend()
     plt.show()
 
 
+def fig4(theta=np.pi/4, delta_m=2.4e-3, L=295):
+    """Oscillated event rate prediction"""
+    bins = np.linspace(0, 10, 201)
+    bin_midpoints = np.arange(0.025, 10, 0.05)
+    u = [theta, delta_m]
 
-fig3()
+    p = [oscillation_probability(u, L, i) for i in bin_midpoints]
+    lambda_i = p * unosc_flux_prediction
+
+    fig = plt.figure(figsize=(12, 8))
+    plt.grid(alpha=0.3)
+    plt.title("Oscillated event rate prediction $\lambda_i (\\bf{u}\\sf)$")
+    plt.xlabel('Energy (GeV)')
+    plt.ylabel('Number of events')
+
+    plt.hist(bins[:-1], bins, weights=lambda_i, label="${\\theta}_{23} = $"+str(theta/np.pi)+"$\pi$ \n"
+                                        "${\\Delta}m_{23}^{2} = $"+str(delta_m)+" \n"
+                                        "$L = $"+str(L))
+
+
+    plt.hist(bins[:-1], bins, weights=data, label="${\\theta}_{23} = $"+"???"+" \n"
+                                        "${\\Delta}m_{23}^{2} = $"+"???"+" \n"
+                                        "$L = $"+"295")
+    plt.legend()
+    plt.show()
+
+
+def fig5(delta_m=2.4e-3):
+    """NLL against theta"""
+
+    # theta range 0 to 2pi
+    theta_vals = np.linspace(0, 2*np.pi, 1000)
+
+    # u values to calculate NPP for
+    u_vals = [[i, delta_m] for i in theta_vals]
+
+    NPP_vals = [negative_log_likelihood(u=i) for i in u_vals]
+
+    fig = plt.figure(figsize=(12, 8))
+    plt.grid(alpha=0.3)
+    plt.title("NLL against $\\theta_{23}$")
+    plt.xlabel('$\\theta_{23}$ ($\pi$ rads)')
+    plt.ylabel('Negative log likelihood')
+
+
+    plt.plot(theta_vals/np.pi, NPP_vals)
+
+    plt.show()
+
+
+fig5()
+
+fig4(theta=0.5 * np.pi)
+fig4(theta=0.25 * np.pi)
+
+
+
