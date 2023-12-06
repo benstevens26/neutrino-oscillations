06932b2dfef462c4640c2c06cf8435add53d43be
Ben <benstevens2609@gmail.com>
2023-12-06 11:56:32 +0000

    amended functions

--- main.py
+++ main.py
@@ -6,43 +6,41 @@ import numpy as np
 from numpy import sin
 from scipy.special import factorial
 
+
 # import data
 unosc_flux_prediction = np.loadtxt("bs521.txt", skiprows=204)
 data = np.loadtxt("bs521.txt", skiprows=2, max_rows=200)
 
+# constant of experiment
+L = 295
 
-def oscillation_probability(u, L, E):
+
+def oscillation_probability(u, E):
     """ Return survival probability of muon neutrino
 
-    :param u: list containing mixing angle and squared mass difference
-    :param L: distance travelled between muon neutrino source and detector
+    :param u: list of parameters [mixing_angle (rad), squared_mass_difference (eV^2)]
     :param E: muon neutrino energy (GeV)
-    :return: probability that muon neutrino has not oscillated
+    :return: muon neutrino survival probability
     """
-    theta = u[0]
-    delta_m = u[1]
 
-    return 1 - (sin(2*theta)**2 * sin(1.267*delta_m*L/E)**2)
+    return 1 - (sin(2*u[0])**2 * sin(1.267*u[1]*L/E)**2)
 
 
 def get_lambda(u):
-    """ Return the oscillated event rate prediction
+    """ Return the oscillated event rate prediction (lambda)
 
     :param u: list of parameters [mixing_angle, squared_mass_difference]
-    :return: oscillated event rate prediction lambda_i
+    :return: oscillated event rate prediction (lambda)
     """
     bin_midpoints = np.arange(0.025, 10, 0.05)  # using bin midpoints as energies
-    p = [oscillation_probability(u, 295, i) for i in bin_midpoints]
+    p = [oscillation_probability(u, i) for i in bin_midpoints]
     return p * unosc_flux_prediction
 
 
 def negative_log_likelihood(u):
     """ Return negative log likelihood for given parameters
 
-    :param mixing_angle:
-    :param squared_mass_difference:
-    :param m: data of muon neutrino events
-    :param lambda_i: oscillated event rate prediction
+    :param u: list of parameters [mixing_angle, squared_mass_difference]
     :return: negative log likelihood of given parameters
     """
     lambda_vals = get_lambda(u)
@@ -51,73 +49,215 @@ def negative_log_likelihood(u):
     return nll
 
 
-def negative_log_likelihood_2(u):
-    """ Return negative log likelihood for given parameters
+def nll2(u):
+    """ Alternative nll function, ONLY for changes in nll
 
-    :param u: [mixing angle, squared mass difference]
-    :param m: data of muon neutrino events
-    :param lambda_i: oscillated event rate prediction
-    :return: negative log likelihood of given parameters
+    :param u: list of parameters [mixing_angle, squared_mass_difference]
+    :return: negative log likelihood
     """
+
     lambda_vals = get_lambda(u)
     m = data
-    nll = 2 * np.sum(lambda_vals - (m * np.log(lambda_vals)))  # without factorial term
-
+    nll = 2 * np.sum(lambda_vals - (m * np.log(lambda_vals)))
     return nll
 
 
-def parabolic_minimiser(f: callable, x1: float, x2: float, x3: float,
+
+def nll_error(u_min):
+    """ Return error on nll_min
+
+    :param u_min: u minima
+    :return: (theta_lb, theta_ub)
+    """
+
+    theta_min = u_min[0]
+    nll_min = negative_log_likelihood(u_min)
+
+    epsilon = 0
+    while (negative_log_likelihood([theta_min + epsilon, u_min[1]]) - nll_min) < 1.0:
+        epsilon += 1e-5
+
+    theta_ub = theta_min + epsilon
+
+    epsilon = 0
+    while (negative_log_likelihood([theta_min - epsilon, u_min[1]]) - nll_min) < 1.0:
+        epsilon += 1e-5
+
+    theta_lb = theta_min - epsilon
+
+    return theta_lb, theta_ub
+
+
+def gen_points(domain):
+    """Generate 3 points within the domain"""
+    domain_points = np.linspace(domain[0], domain[1], 1000)
+    x1 = domain_points[np.random.randint(0, 333)]
+    x2 = domain_points[np.random.randint(333, 667)]
+    x3 = domain_points[np.random.randint(666, 999)]
+    return x1, x2, x3
+
+
+def parabolic_minimiser(f, x1: float, x2: float, x3: float, args=(),
                         tol=1e-6, max_iter=1e6, full_output=False):
     """ Return local minima via parabolic interpolation, where possible
 
     :param f: function to be minimised
-    :param x1: initial value 1
+    :param x1: initial guess 1
     :param x2: initial value 2
     :param x3: initial value 3
+    :param args: tuple of arguments to be passed to the function f
     :param tol: criteria to stop iteration
     :param max_iter: maximum number of iterations
-    :param full_output: [x_min, f(x_min)]
+    :param full_output: returns [x, f(x)] for each interpolated minima
     :return: x_min or result after max_iter
     """
 
     max_iter = int(max_iter)
+    if max_iter <= 0:
+        raise ValueError("max_iter must be greater than 0")
+
+    points = []
+    x_list = []
+    y_list = []
 
     for i in range(max_iter):
 
-        y1, y2, y3 = f(x1), f(x2), f(x3)
+        y1, y2, y3 = f(x1, *args), f(x2, *args), f(x3, *args)
         x_list = [x1, x2, x3]
         y_list = [y1, y2, y3]
-        y_min = min([y_list])
+        y_min = min(y_list)
+        x_min = x_list[y_list.index(y_min)]
 
         top = ((x3*x3)-(x2*x2))*y1 + ((x1*x1)-(x3*x3))*y2 + ((x2*x2)-(x1*x1))*y3
         bottom = (x3-x2)*y1 + (x1-x3)*y2 + (x2-x1)*y3
 
-        x4 = 0.5 * (top / bottom)  # minima of parabola
-        y4 = f(x4)
-
-        if y4 >= y_min:
-            print("Not decreasing, returning")
-            return x4
-
-        if abs(y4 - y_min) < tol:
-            print("Success in", i, "iterations")
-
-            if full_output:
-                return [x4, y4]
-
-            return x4
+        x4 = 0.5 * (top / bottom)  # x4 is minima of parabola
+        y4 = f(x4, *args)
+        points.append([x4, y4])
 
         x_list.append(x4)
         y_list.append(y4)
+
+        if x4 == x1 or x4 == x2 or x4 == x3:
+            print("Degenerate case encountered, regenerating initial guesses")
+            x1, x2, x3 = gen_points([x1, x3])
+            continue
+
+        if abs(x4 - x_min) < tol:
+            print("Success in", i+1, "iteration(s)")
+
+            if full_output:
+                curvature = (2*y1)/((x2-x1)*(x3-x1)) - (2*y2)/((x2-x1)*(x3-x2)) + (2*y3)/((x3-x1)*(x3-x2))
+                return x4, points, curvature
+
+            return x4
+
         max_index = y_list.index(max(y_list))
         x_list.pop(max_index)
 
         x1, x2, x3 = x_list
 
-    print("Maximum Iterations Reached")
-    min_index = min(y_list)
+    print(max_iter,"iterations reached")
+    min_index = y_list.index(min(y_list))
     x_min = x_list[min_index]
+
+    if full_output:
+        return points
+
     return x_min
+
+
+def parabolic_minimiser_nd(f, axis, x0, xrange: float, args=(),
+                          tol=1e-6, max_iter=1e6, full_output=False):
+    """ Return local minima via parabolic interpolation along a given axis
+
+    :param f: function to be minimised
+    :param axis: axis (index i) on which to minimise f(x_0, x_1, ... x_i)
+    :param x0: initial guess of minima [x0_min, x1_min ... xi_min]
+    :param args: tuple of arguments to be passed to the function f
+    :param tol: criteria to stop iteration
+    :param max_iter: maximum number of iterations
+    :param full_output: returns [x, f(x)] for each interpolated minima
+    :return: x_min or result after max_iter along axis x_i
+    """
+
+    max_iter = int(max_iter)
+    if max_iter <= 0:
+        raise ValueError("max_iter must be greater than 0")
+
+    points = []
+    x_list = []
+    y_list = []
+
+    x1 = x0.copy()
+    x2 = x0.copy()
+    x3 = x0.copy()
+
+    a, b, c = gen_points([x0[axis] - xrange/2, x0[axis] + xrange/2])
+
+    x1[axis] = a
+    x2[axis] = b
+    x3[axis] = c
+
+    initial_vals = [x1, x2, x3]
+
+    for i in range(max_iter):
+
+        y1, y2, y3 = f(x1, *args), f(x2, *args), f(x3, *args)
+        x_list = [x1, x2, x3]
+        y_list = [y1, y2, y3]
+        y_min = min(y_list)
+        x_min = x_list[y_list.index(y_min)]
+
+        top = (((x3[axis]*x3[axis])-(x2[axis]*x2[axis]))*y1
+               + ((x1[axis]*x1[axis])-(x3[axis]*x3[axis]))*y2
+               + ((x2[axis]*x2[axis])-(x1[axis]*x1[axis]))*y3)
+        bottom = ((x3[axis]-x2[axis])*y1 + (x1[axis]-x3[axis])*y2
+                  + (x2[axis]-x1[axis])*y3)
+
+        x4 = x1.copy()  # is only changing along the axis that is being minimised
+        x4[axis] = 0.5 * (top / bottom)  # x4 is minima of parabola in [axis] index
+
+        y4 = f(x4, *args)
+        points.append([x4, y4])
+
+        x_list.append(x4)
+        y_list.append(y4)
+
+        if x4[axis] == x1[axis] or x4[axis] == x2[axis] or x4[axis] == x3[axis]:
+            print("Degenerate case encountered, regenerating initial guesses")
+            a, b, c = gen_points([x1[axis], x3[axis]])
+            x1[axis] = a
+            x2[axis] = b
+            x3[axis] = c
+            continue
+
+        if abs(x4[axis] - x_min[axis]) < tol:
+            print("Success in", i+1, "iteration(s)")
+
+            # if full_output:
+            #     curvature = (2*y1)/((x2-x1)*(x3-x1)) - (2*y2)/((x2-x1)*(x3-x2)) + (2*y3)/((x3-x1)*(x3-x2))
+            #     return x4, points, curvature
+
+            return x4
+
+        max_index = y_list.index(max(y_list))
+        x_list.pop(max_index)
+
+        x1, x2, x3 = x_list
+
+    print(max_iter,"iterations reached")
+    min_index = y_list.index(min(y_list))
+    x_min = x_list[min_index]
+
+    if full_output:
+        return points
+
+    return x_min
+
+
+def univariate_minimiser():
+    """ Return minima by successive parabolic minimisation along each axis"""
 
 
 
